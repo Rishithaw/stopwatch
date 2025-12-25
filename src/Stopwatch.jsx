3ab638e146c9a5158d1ab0097765826487d2c747
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-12-25 16:37:59 -0600

    Added HTML

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -23,8 +23,18 @@ function Stopwatch() {
   }
 
   function formatTime() {
-
+    return '00:00:00';
   }
-  return (<></>);
+
+  return (
+    <div className='stopwatch'>
+      <div className='display'>{formatTime()}</div>
+      <div className='controls'>
+        <button onClick={start} className='start-button'>Start</button>
+        <button onClick={stop} className='stop-button'>Stop</button>
+        <button onClick={reset} className='reset-button'>Reset</button>
+      </div>
+    </div>
+  );
 }
 export default Stopwatch;
