c190a30b5cf2c9f6167e64602fefb38be44301ed
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-12-25 17:08:35 -0600

    Added functionality

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -7,23 +7,43 @@ function Stopwatch() {
   const startTimeRef = useRef(null);
 
   useEffect(() => {
+    if (isRunning) {
+      intervalRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current);
+      }, 10);
+    }
 
+    return () => {
+      clearInterval(intervalRef.current);
+    }
   }, [isRunning]);
 
   function start() {
-
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
   }
 
   function stop() {
-
+    setIsRunning(false);
   }
 
   function reset() {
-
+    setElapsedTime(0);
+    setIsRunning(false);
   }
 
   function formatTime() {
-    return '00:00:00';
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60)% 60);
+    let seconds = Math.floor(elapsedTime / (1000) % 60);
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+    hours = String(hours).padStart(2, '0');
+    minutes = String(minutes).padStart(2, '0');
+    seconds = String(seconds).padStart(2, '0');
+    milliseconds = String(milliseconds).padStart(2, '0');
+
+    return `${minutes}:${seconds}:${milliseconds}`;
   }
 
   return (
