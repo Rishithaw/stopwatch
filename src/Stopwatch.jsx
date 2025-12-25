769980630da6d679047553544d00e900888449f0
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-12-25 16:21:35 -0600

    Added state variables

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,6 +1,10 @@
 import React, {useState, useEffect, uesRef} from 'react';
 
 function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalRef = uesRef(null);
+  const startTimeRef = uesRef(null);
   return (<></>);
 }
 
