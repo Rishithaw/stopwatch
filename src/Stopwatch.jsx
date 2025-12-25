14a7cb1c98cbb831bd5908a61a61b4c1e4e52d76
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-12-25 16:32:59 -0600

    Added functions

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,11 +1,30 @@
-import React, {useState, useEffect, uesRef} from 'react';
+import React, {useState, useEffect, uesRef, use} from 'react';
 
 function Stopwatch() {
   const [isRunning, setIsRunning] = useState(false);
   const [elapsedTime, setElapsedTime] = useState(0);
   const intervalRef = uesRef(null);
   const startTimeRef = uesRef(null);
+
+  useEffect(() => {
+
+  }, [isRunning]);
+
+  function start() {
+
+  }
+
+  function stop() {
+
+  }
+
+  function reset() {
+
+  }
+
+  function formatTime() {
+
+  }
   return (<></>);
 }
-
 export default Stopwatch;
